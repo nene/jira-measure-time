a3053d9d7d5edfade5996a8e8b55f27284c1d056
Rene Saarsoo <nene@triin.net>
2015-02-03 11:53:57 +0200

    Implement state saving to localStorage.

--- jira-measure-time.js
+++ jira-measure-time.js
@@ -6,6 +6,53 @@ $(function(){
         return;
     }
 
+    function TimeTracker(issueNr) {
+        this.id = "timetracker-"+issueNr;
+        this.load();
+    }
+    TimeTracker.prototype = {
+        start: function() {
+            this.data.startTime = new Date().getTime();
+            this.save();
+        },
+        stop: function() {
+            this.data.duration = this.getRunningDuration();
+            delete this.data.startTime;
+            this.save();
+        },
+
+        getStartTime: function() {
+            return this.data.startTime;
+        },
+        getDuration: function() {
+            return this.data.duration || 0;
+        },
+
+        isRunning: function() {
+            return !!this.data.startTime;
+        },
+        getRunningDuration: function() {
+            if (!this.isRunning()) {
+                return this.getDuration();
+            }
+            return this.getDuration() + (new Date() - this.data.startTime);
+        },
+
+        save: function() {
+            localStorage[this.id] = JSON.stringify(this.data);
+        },
+        load: function() {
+            try {
+                this.data = JSON.parse(localStorage[this.id]);
+            }
+            catch (e) {
+                this.data = {};
+            }
+        }
+    };
+
+    var timeTracker = new TimeTracker(extractIssueNr(document.location.href));
+
     $("head").append(
         "<style>" +
         ".jmt-start { color: green; }" +
@@ -19,37 +66,46 @@ $(function(){
 
     var startBtn = $("<button class='jmt-start'>Start</button>");
     startBtn.on("click", function(){
-        startBtn.hide();
-        stopBtn.show();
-
-        startTime = new Date();
+        timeTracker.start();
+        updateButtonsVisibility();
     });
 
     var stopBtn = $("<button class='jmt-stop'>Stop</button>");
     stopBtn.on("click", function(){
-        stopBtn.hide();
-        startBtn.show();
-
-        totalDuration = getTotalDuration();
-        displayDuration(totalDuration);
-        startTime = undefined;
+        timeTracker.stop();
+        updateButtonsVisibility();
+        displayDuration(timeTracker.getDuration());
     });
-    stopBtn.hide();
 
     var durationLabel = $("<span class='jmt-duration'></span>");
+    displayDuration(timeTracker.getRunningDuration());
+
+    updateButtonsVisibility();
 
     toolbar.append(startBtn);
     toolbar.append(stopBtn);
     toolbar.append(durationLabel);
 
     setInterval(function(){
-        if (startTime) {
-            displayDuration(getTotalDuration());
+        if (timeTracker.isRunning()) {
+            displayDuration(timeTracker.getRunningDuration());
         }
     }, 250);
 
-    function getTotalDuration() {
-        return totalDuration + (new Date() - startTime);
+    function updateButtonsVisibility() {
+        if (timeTracker.isRunning()) {
+            startBtn.hide();
+            stopBtn.show();
+        }
+        else {
+            startBtn.show();
+            stopBtn.hide();
+        }
+    }
+
+    function extractIssueNr(url) {
+        var matches = url.match(/browse\/([\w\d-]+)/);
+        return matches[1];
     }
 
     function displayDuration(ms) {
